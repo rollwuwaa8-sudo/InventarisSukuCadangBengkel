08c44557a75bbe079d9b87eca83d2c962e318c17
rollwuwaa8-sudo <rollwuwaa8@gmail.com>
2026-05-08 17:50:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "URL_WEB_APP_BARU_ANDA"; // Ganti dengan URL Deployment Anda
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx9en3IDhmiHSHyH5HQbdpF6ZhCyQEBt8Nqpm9PKCmL-0AKYcQ0RIg7Wfw11G1xrkCNsA/exec"; // Ganti dengan URL Deployment Anda
 
 const form = document.getElementById('inventory-form');
 const tableBody = document.getElementById('table-body');
